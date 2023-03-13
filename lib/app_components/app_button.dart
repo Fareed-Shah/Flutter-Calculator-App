e5488c362f86c27d42239651ad56a6fa20b1a17e
Fareed-Shah <syedfareed667@gmail.com>
2023-03-13 22:24:17 +0500

    worked on onPress

--- lib/app_components/app_button.dart
+++ lib/app_components/app_button.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 class AppButton extends StatelessWidget {
   final String label;
   final Color btnColor;
+  final VoidCallback onPress;
   const AppButton(
       {super.key,
       required this.label,
+      required this.onPress,
       this.btnColor = const Color(0xffa5a5a5)});
 
   @override
@@ -13,18 +15,21 @@ class AppButton extends StatelessWidget {
     return Expanded(
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 12.0),
-        child: Container(
-          height: 70,
-          // width: 50,
-          //oxffffa00a
-          decoration: BoxDecoration(color: btnColor, shape: BoxShape.circle),
-          child: Center(
-              child: Text(label.toString(),
-                  style: const TextStyle(
-                      fontFamily: 'TiltWarp Regular',
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16.0,
-                      color: Colors.white))),
+        child: GestureDetector(
+          onTap: onPress,
+          child: Container(
+            height: 70,
+            // width: 50,
+            //oxffffa00a
+            decoration: BoxDecoration(color: btnColor, shape: BoxShape.circle),
+            child: Center(
+                child: Text(label.toString(),
+                    style: const TextStyle(
+                        fontFamily: 'TiltWarp Regular',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                        color: Colors.white))),
+          ),
         ),
       ),
     );
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:calculator_app/app_components/app_button.dart';
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -9,6 +10,17 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var userInput = '';
+  var answer = '';
+
+  void equalPress() {
+    Parser parser = Parser();
+    Expression expression = parser.parse(userInput);
+    ContextModel contextModel = ContextModel();
+
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,88 +36,224 @@ class _HomeState extends State<Home> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.end,
                   crossAxisAlignment: CrossAxisAlignment.end,
-                  children: const [
+                  children: [
                     Align(
                       alignment: Alignment.bottomRight,
                       child: Text(
-                        '0',
-                        style: TextStyle(
+                        userInput.toString(),
+                        style: const TextStyle(
                             color: Colors.white,
                             fontSize: 30.0,
                             fontWeight: FontWeight.bold),
                       ),
                     ),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Text(
+                        answer.toString(),
+                        style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 30.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    )
                   ],
                 ),
               ),
             ),
             Expanded(
-              flex: 2,
+              flex: 3,
               child: Column(
                 children: [
                   Row(
-                    children: const [
-                      AppButton(label: 'AC'),
-                      AppButton(label: '+/-'),
-                      AppButton(label: '%'),
-                      AppButton(label: '/', btnColor: Color(0xffffa00a)),
+                    children: [
+                      AppButton(
+                          label: 'AC',
+                          onPress: () {
+                            setState(() {
+                              userInput = '';
+                              answer = '';
+                            });
+                          }),
+                      AppButton(
+                          label: '+/-',
+                          onPress: () {
+                            setState(() {
+                              userInput += '+/-';
+                            });
+                          }),
+                      AppButton(
+                          label: '%',
+                          onPress: () {
+                            setState(() {
+                              userInput += '%';
+                            });
+                          }),
+                      AppButton(
+                          label: '/',
+                          onPress: () {
+                            setState(() {
+                              userInput += '/';
+                            });
+                          },
+                          btnColor: const Color(0xffffa00a)),
                     ],
                   ),
                   Row(
-                    children: const [
-                      AppButton(label: '7'),
-                      AppButton(label: '8'),
-                      AppButton(label: '0'),
-                      AppButton(label: 'x', btnColor: Color(0xffffa00a)),
+                    children: [
+                      AppButton(
+                          label: '7',
+                          onPress: () {
+                            setState(() {
+                              userInput += '7';
+                            });
+                          }),
+                      AppButton(
+                          label: '8',
+                          onPress: () {
+                            setState(() {
+                              userInput += '8';
+                            });
+                          }),
+                      AppButton(
+                          label: '0',
+                          onPress: () {
+                            setState(() {
+                              userInput += '0';
+                            });
+                          }),
+                      AppButton(
+                          label: 'x',
+                          onPress: () {
+                            setState(() {
+                              userInput += 'x';
+                            });
+                          },
+                          btnColor: const Color(0xffffa00a)),
                     ],
                   ),
                   Row(
-                    children: const [
-                      AppButton(label: '4'),
-                      AppButton(label: '5'),
-                      AppButton(label: '6'),
-                      AppButton(label: '-', btnColor: Color(0xffffa00a)),
+                    children: [
+                      AppButton(
+                          label: '4',
+                          onPress: () {
+                            setState(() {
+                              userInput += '4';
+                            });
+                          }),
+                      AppButton(
+                          label: '5',
+                          onPress: () {
+                            setState(() {
+                              userInput += '5';
+                            });
+                          }),
+                      AppButton(
+                          label: '6',
+                          onPress: () {
+                            setState(() {
+                              userInput += '6';
+                            });
+                          }),
+                      AppButton(
+                          label: '-',
+                          onPress: () {
+                            setState(() {
+                              userInput += '-';
+                            });
+                          },
+                          btnColor: const Color(0xffffa00a)),
                     ],
                   ),
                   Row(
-                    children: const [
-                      AppButton(label: '1'),
-                      AppButton(label: '2'),
-                      AppButton(label: '3'),
-                      AppButton(label: '+', btnColor: Color(0xffffa00a)),
+                    children: [
+                      AppButton(
+                          label: '1',
+                          onPress: () {
+                            setState(() {
+                              userInput += '1';
+                            });
+                          }),
+                      AppButton(
+                          label: '2',
+                          onPress: () {
+                            setState(() {
+                              userInput += '2';
+                            });
+                          }),
+                      AppButton(
+                          label: '3',
+                          onPress: () {
+                            setState(() {
+                              userInput += '3';
+                            });
+                          }),
+                      AppButton(
+                          label: '+',
+                          onPress: () {
+                            setState(() {
+                              userInput += '+';
+                            });
+                          },
+                          btnColor: const Color(0xffffa00a)),
                     ],
                   ),
                   Row(
-                    children: const [
-                      AppButton(label: '0'),
-                      AppButton(label: '.'),
-                      AppButton(label: 'DEL'),
-                      AppButton(label: '=', btnColor: Color(0xffffa00a)),
+                    children: [
+                      AppButton(
+                          label: '0',
+                          onPress: () {
+                            setState(() {
+                              userInput += '0';
+                            });
+                          }),
+                      AppButton(
+                          label: '.',
+                          onPress: () {
+                            setState(() {
+                              userInput += '.';
+                            });
+                          }),
+                      AppButton(
+                          label: 'DEL',
+                          onPress: () {
+                            setState(() {
+                              userInput += 'DEL';
+                            });
+                          }),
+                      AppButton(
+                          label: '=',
+                          onPress: () {
+                            setState(() {
+                              userInput += '=';
+                            });
+                          },
+                          btnColor: const Color(0xffffa00a)),
                     ],
                   ),
                   const SizedBox(
                     height: 30.0,
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      RichText(
-                          text: const TextSpan(children: [
-                        TextSpan(
-                          text: 'Calculator App Design By :',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        TextSpan(
-                          text: ' Fareed Shah',
-                          style: TextStyle(
-                              fontSize: 25.0,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold),
-                        )
-                      ])),
-                    ],
-                  )
+                  // Row(
+                  //   mainAxisAlignment: MainAxisAlignment.center,
+                  //   crossAxisAlignment: CrossAxisAlignment.center,
+                  //   children: [
+                  //     RichText(
+                  //         text: const TextSpan(children: [
+                  //       TextSpan(
+                  //         text: 'Calculator App Design By :',
+                  //         style: TextStyle(color: Colors.white),
+                  //       ),
+                  //       TextSpan(
+                  //         text: ' Fareed Shah',
+                  //         style: TextStyle(
+                  //             fontSize: 25.0,
+                  //             color: Colors.white,
+                  //             fontWeight: FontWeight.bold),
+                  //       )
+                  //     ])),
+                  //   ],
+                  // )
                 ],
               ),
             ),
